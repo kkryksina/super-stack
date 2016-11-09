2ccb63c9cc4ed4eabc195dd59a5680c05e48eecc
Kseniya Kryksina <kkryksina@yandex.ru>
2016-11-09 22:52:55 +0300

    Change entrypoint file with ES6

--- index.js
+++ index.js
@@ -2,34 +2,38 @@
 
 const assert = require('assert');
 
-function Node(value) {
-    this.value = value;
+class Node {
+    constructor(value) {
+        this.value = value;
+    }
 }
 
-function Stack() {
-    this._top = null;
-}
+class Stack {
+    constructor() {
+        this._top = null;
+    }
 
-Stack.prototype.isEmpty = function() {
-    return this._top == null;
-}
+    isEmpty() {
+        return this._top == null;
+    }
 
-Stack.prototype.push = function(value) {
-    const newNode = new Node(value);
-    newNode.next = this._top;
-    this._top = newNode;
-}
+    push(value) {
+        const newNode = new Node(value);
+        newNode.next = this._top;
+        this._top = newNode;
+    }
 
-Stack.prototype.pop = function() {
-    assert(!this.isEmpty(), 'Error: stack is empty');
-    const topValue = this._top.value;
-    this._top = this._top.next;
-    return topValue;
-}
+    pop() {
+        assert(!this.isEmpty(), 'Error: stack is empty');
+        const topValue = this._top.value;
+        this._top = this._top.next;
+        return topValue;
+    }
 
-Stack.prototype.peek = function() {
-    assert(!this.isEmpty(), 'Error: stack is empty');
-    return this._top.value;
+    peek() {
+        assert(!this.isEmpty(), 'Error: stack is empty');
+        return this._top.value;
+    }
 }
 
 module.exports = Stack;
