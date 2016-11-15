d28e5ffdafac0de18b75f9e5cbeffbf67e1500b4
Kseniya Kryksina <kkryksina@yandex.ru>
2016-11-15 23:27:18 +0300

    Add JSDoc to entrypint file

--- index.js
+++ index.js
@@ -2,27 +2,48 @@
 
 const assert = require('assert');
 
+/** Class representing a Node. */
 class Node {
+    /**
+     * Create a Node.
+     * @param value - The  value.
+     */
     constructor(value) {
         this.value = value;
     }
 }
 
+/** Class representing a Stack. */
 class Stack {
+    /**
+     * Create a Stack.
+     */
     constructor() {
         this._top = null;
     }
 
+    /**
+     * Сheck the emptiness of the stack.
+     * @return {boolean} True or false.
+     */
     isEmpty() {
         return this._top === null;
     }
-
+    
+    /**
+     * Put the value on top of the stack.
+     * @param value - The  value.
+     */
     push(value) {
         const newNode = new Node(value);
         newNode.next = this._top;
         this._top = newNode;
     }
-
+    
+    /**
+     * Pop and return the value from top of the stack.
+     * @return The value.
+     */
     pop() {
         assert(!this.isEmpty(), 'stack is empty');
 
@@ -30,7 +51,12 @@ class Stack {
         this._top = this._top.next;
         return topValue;
     }
-
+    
+    /**
+     * Return the value from top of the stack.
+     * @return The value.
+     * 
+     */
     peek() {
         assert(!this.isEmpty(), 'stack is empty');
 
